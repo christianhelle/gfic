8a07380df89c0cacc8d4d0fc7266d75fe39bf331
Christian Resma Helle <che@cloudeon.com>
2019-05-18 01:53:47 +0200

    Add line breaks to logs where needed

--- src/gfic/Program.fs
+++ src/gfic/Program.fs
@@ -1,15 +1,17 @@
 ﻿// Learn more about F# at http://fsharp.org
 
+open System
 open System.IO
 
 open ImageProcessor
 
 [<EntryPoint>]
 let main argv =
-    printfn "gfic image converter"
+    printfn "%sgfic image converter" Environment.NewLine
     let files = Directory.GetFiles(Directory.GetCurrentDirectory(), "*.jpg")
-    printfn "Found %d file(s)" files.Length
+    printfn "Found %d file(s)%s" files.Length Environment.NewLine
     files
     |> Array.map Path.GetFullPath 
-    |> Array.iter convertToGrayScale 
+    |> Array.iter convertToGrayScale
+    printfn "%s" Environment.NewLine 
     0 // return an integer exit code
