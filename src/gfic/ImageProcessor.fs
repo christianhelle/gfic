c55fbe86a800490e99de961bd9b1bd2b50724cff
Christian Resma Helle <che@cloudeon.com>
2019-05-17 23:38:27 +0200

    Implement grayscale image conversion using ImageSharp

---
+++ src/gfic/ImageProcessor.fs
@@ -0,0 +1,13 @@
+﻿module ImageProcessor
+
+open System.IO
+open SixLabors.ImageSharp
+open SixLabors.ImageSharp.Processing
+
+let convertToGrayScale file =
+    let fi = new FileInfo(file)
+    let dir = Directory.CreateDirectory(Path.Combine(fi.DirectoryName, "grayscale"))
+    let output = Path.Combine(dir.FullName, fi.Name)
+    let image = Image.Load(file)
+    image.Mutate(fun x -> x.Grayscale() |> ignore)
+    image.Save(output)
---
+++ src/gfic/Program.fs
@@ -0,0 +1,17 @@
+﻿// Learn more about F# at http://fsharp.org
+
+open System.IO
+
+open ImageProcessor
+
+[<EntryPoint>]
+let main argv =
+    printfn "gfic image converter"
+    let imageFolder = "..\\..\\..\\..\\..\\images"
+    let imagePath = Path.Combine(Directory.GetCurrentDirectory(), imageFolder)
+    let files = Directory.GetFiles(imagePath, "*.jpg")
+    printfn "Found %d file(s)" files.Length
+    files
+    |> Array.map Path.GetFullPath 
+    |> Array.iter convertToGrayScale 
+    0 // return an integer exit code
