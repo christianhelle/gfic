4dcd548c341e82facf83d644c782cd4cb7d0399e
Christian Resma Helle <che@cloudeon.com>
2019-05-18 01:53:25 +0200

    Log processing time used per image

--- src/gfic/ImageProcessor.fs
+++ src/gfic/ImageProcessor.fs
@@ -1,14 +1,16 @@
 ﻿module ImageProcessor
 
+open System.Diagnostics
 open System.IO
 open SixLabors.ImageSharp
 open SixLabors.ImageSharp.Processing
 
 let convertToGrayScale file =
+    let sw = Stopwatch.StartNew()
     let fi = new FileInfo(file)
     let dir = Directory.CreateDirectory(Path.Combine(fi.DirectoryName, "grayscale"))
     let output = Path.Combine(dir.FullName, fi.Name)
     let image = Image.Load(file)
     image.Mutate(fun x -> x.Grayscale() |> ignore)
     image.Save(output)
-    printfn "%s - Done!" output
+    printfn "%O - %s" sw.Elapsed output
