291179d8e793f1fdf2792172ef959b77347bcff2
Christian Resma Helle <christian.helle@outlook.com>
2019-05-18 20:55:40 +0200

    Rename convertToGrayScale to ToGrayScale

--- src/gfic/Program.fs
+++ src/gfic/Program.fs
@@ -1,9 +1,8 @@
 ﻿open System
 open System.Diagnostics
 open System.IO
+open System.Threading
 open System.Threading.Tasks
-
-open ImageProcessor
 
 [<EntryPoint>]
 let main argv =
@@ -11,8 +10,10 @@ let main argv =
     let sw = Stopwatch.StartNew()
     let crlb = Environment.NewLine
     for format in ["*.jpg"; "*.png"] do
-        let files = Directory.GetFiles(options.InputDir, format)
-        Parallel.ForEach(files, fun file -> 
-            convertToGrayScale(file, options.OutputDir)) |> ignore
+        Directory.GetFiles(options.InputDir, format)
+        |> fun files -> 
+            Parallel.ForEach(files, fun file -> 
+                ImageProcessor.ToGrayScale(file, options.OutputDir)) 
+        |> ignore
     printfn "%sTotal time: %O%s" crlb sw.Elapsed crlb 
     0
