361940ec5b898afd54bb0e7558eadf203298386f
Christian Resma Helle <che@cloudeon.com>
2019-05-18 12:20:32 +0200

    Add support for converting PNG files

--- src/gfic/Program.fs
+++ src/gfic/Program.fs
@@ -1,6 +1,4 @@
-﻿// Learn more about F# at http://fsharp.org
-
-open System
+﻿open System
 open System.IO
 
 open ImageProcessor
@@ -8,10 +6,10 @@ open ImageProcessor
 [<EntryPoint>]
 let main argv =
     printfn "%sgfic image converter" Environment.NewLine
-    let files = Directory.GetFiles(Directory.GetCurrentDirectory(), "*.jpg")
-    printfn "Found %d file(s)%s" files.Length Environment.NewLine
-    files
-    |> Array.map Path.GetFullPath 
-    |> Array.iter convertToGrayScale
-    printfn "%s" Environment.NewLine 
+    for f in ["*.jpg"; "*.png"] do
+        Directory.GetFiles(Directory.GetCurrentDirectory(), f)
+        |> Array.map Path.GetFullPath 
+        |> Array.iter convertToGrayScale
+    
+    printfn "%s" Environment.NewLine
     0 // return an integer exit code
