ed8af6fdf33f7f7356116c3cb87bc08e5432ad45
Christian Resma Helle <christian.helle@outlook.com>
2019-05-18 15:38:47 +0200

    Use Parallel.ForEach to process all images as fast as possible (#1)

--- src/gfic/Program.fs
+++ src/gfic/Program.fs
@@ -1,6 +1,7 @@
 ﻿open System
 open System.Diagnostics
 open System.IO
+open System.Threading.Tasks
 
 open ImageProcessor
 
@@ -8,9 +9,8 @@ open ImageProcessor
 let main argv =
     let sw = Stopwatch.StartNew()
     let crlb = Environment.NewLine
-    for f in ["*.jpg"; "*.png"] do
-        Directory.GetFiles(Directory.GetCurrentDirectory(), f)
-        |> Array.map Path.GetFullPath 
-        |> Array.iter convertToGrayScale    
+    for format in ["*.jpg"; "*.png"] do
+        let files = Directory.GetFiles(Directory.GetCurrentDirectory(), format)
+        Parallel.ForEach(files, fun file -> convertToGrayScale(file)) |> ignore
     printfn "%sTotal time: %O%s" crlb sw.Elapsed crlb 
     0 // return an integer exit code
