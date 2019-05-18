1759180b6676f28fc182355a2ff9f76fc0142bf8
Christian Resma Helle <christian.helle@outlook.com>
2019-05-18 20:54:45 +0200

    Create output folder upfront if it doesn't exist

--- src/gfic/CLIArguments.fs
+++ src/gfic/CLIArguments.fs
@@ -24,21 +24,27 @@ with
             | Input _ -> "specify a folder for source images"
             | Output _ -> "specify the output folder."
 
-let validate = function
+let Validate = function
     | { InputDir = "" } -> "ERROR: missing parameter '--input'." |> Error
     | o -> o |> Ok
+
+let GetPath dir =
+    Directory.GetCurrentDirectory()
+    |> fun d -> Path.Combine(d, dir)
+    |> Directory.CreateDirectory
+    |> fun d -> d.FullName
 
 let Parse progName args = 
     let exiter = ProcessExiter () :> IExiter
     let parser = ArgumentParser.Create<CLIArguments>(progName, errorHandler = exiter)
     let rec loop o = function
         | Input u::t -> t |> loop { o with InputDir = Path.GetDirectoryName(u) }
-        | Output u::t -> t |> loop { o with OutputDir = Path.GetDirectoryName(u) }
+        | Output u::t -> t |> loop { o with OutputDir = GetPath(u) }
         | [] -> o
     parser.Parse args
     |> (fun a -> a.GetAllResults())
     |> loop Options.Empty
-    |> validate
+    |> Validate
     |> function
         | Ok o -> o
         | Error e ->
