94a36bd854b520a0d4257e00fb7005fbd34f6d03
Christian Helle <christian.helle@outlook.com>
2020-12-30 21:21:02 +0100

    Extract Logger module

---
+++ src/gfic/Logger.fs
@@ -0,0 +1,26 @@
+module Logger
+
+open System
+open System.Diagnostics
+open System.IO
+open System.Text
+open System.Threading.Tasks
+open Exceptionless
+open System.Security.Cryptography
+
+let GetUserIdentity =
+    try Environment.UserName + "@" + Environment.MachineName
+    with | ex -> Environment.UserName + "@localhost"
+
+let GetAnonymousIdentity =
+    let sb = new StringBuilder()
+    GetUserIdentity
+        |> Encoding.UTF8.GetBytes
+        |> (new SHA256Managed()).ComputeHash
+        |> fun hash -> for b in hash do sb.Append(b.ToString("x2")) |> ignore
+    sb.ToString()
+
+let Setup =
+    GetAnonymousIdentity |> fun sha -> ExceptionlessClient.Default.Configuration.SetUserIdentity(sha, sha.Substring(0,7))
+    ExceptionlessClient.Default.Configuration.UseSessions()
+    ExceptionlessClient.Default.Startup("4f8WTcyL2cDDRJ90a6khQ1CIGv1lee2WfS3M8JsJ")
