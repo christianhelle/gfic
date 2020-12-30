7c2d529eee631d82309415c8ca937861f48e76f8
Christian Helle <christian.helle@outlook.com>
2020-12-31 00:17:56 +0100

    Implement string to enum extension method

---
+++ src/gfic/Extensions.fs
@@ -0,0 +1,10 @@
+﻿namespace Extensions
+
+open System
+
+[<AutoOpen>]
+module FsExtensions =
+    type String with
+        member this.ToEnum<'a when 'a :> Enum and 'a : struct and 'a : (new: unit -> 'a)> (?ignoreCase) =
+            let ok, v = Enum.TryParse<'a>(this, defaultArg ignoreCase true)
+            if ok then Some v else None
