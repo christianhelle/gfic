766314d590d25dd5f79c1c009b231be939017996
Christian Helle <christian.helle@outlook.com>
2021-01-03 13:39:44 +0100

    Add CLIArguments tests

---
+++ src/gfic.tests/CLIArgumentsTests.fs
@@ -0,0 +1,28 @@
+﻿module ``CLIArguments Tests``
+
+open Xunit
+open System
+open System.IO
+
+let GetArgs =
+    [| "--input";"../../../../../images/original/";"--effect";"all";"--no-logging" |]
+
+[<Fact>]
+let ``Parse returns not null`` () =
+    GetArgs |> CLIArguments.Parse |> Assert.NotNull
+    
+[<Fact>]
+let ``Parse reads --no-logging`` () =
+    GetArgs |> CLIArguments.Parse |> fun o -> Assert.True(o.NoLogging)
+    
+[<Fact>]
+let ``Parse reads --effect`` () =
+    GetArgs |> CLIArguments.Parse |> fun o -> Assert.Equal("all", o.Effect)
+    
+[<Fact>]
+let ``Parse reads --input`` () =
+    GetArgs 
+    |> CLIArguments.Parse
+    |> fun o -> 
+        let expected = Path.GetDirectoryName("../../../../../images/original/")
+        Assert.Equal(expected, o.InputDir)
