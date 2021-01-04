4b2dad7df77d3183f5cf1025c4c5ae40d2dcc93a
Christian Helle <christian.helle@outlook.com>
2021-01-04 09:00:04 +0100

    Add ImageResizer tests

---
+++ src/gfic.tests/ImageResizerTests.fs
@@ -0,0 +1,19 @@
+﻿module ``ImageResizer Tests``
+
+open Xunit
+open System.IO
+
+open SixLabors.ImageSharp
+
+[<Fact>]
+let ``Can Resize`` () = 
+    EmbeddedResource.ExtractAll
+    for name in ["guitar1.jpg";"guitar2.jpg"] do
+        Path.Combine(Directory.GetCurrentDirectory(), name)
+        |> Image.Load
+        |> fun img -> 
+            let height = img.Height
+            let width = img.Width
+            ImageResizer.Resize(50, img)
+            Assert.True(img.Height < height)
+            Assert.True(img.Width < width)
